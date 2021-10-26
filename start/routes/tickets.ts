193c24b8a2ba1c45f729e2a640b32afadcd0e634
rfxct <xbrcvar@gmail.com>
2021-10-26 08:26:11 -0300

    remove routes

--- start/routes/tickets.ts
+++
@@ -1,7 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.group(() => {
-  // Get, Create, Update, Delete
-  Route.resource('tickets', 'TicketsController').apiOnly()
-  Route.resource('tickets/:ticketId/messages', 'TicketMessagesController').apiOnly()
-}).middleware('auth')
--- start/routes/users.ts
+++
@@ -1,8 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.group(() => {
-  // Get, Update, Delete
-  Route.resource('users', 'UsersController').apiOnly().except(['store'])
-
-  Route.get('users/:id/tickets', 'UsersController.showTickets')
-}).middleware('auth')
