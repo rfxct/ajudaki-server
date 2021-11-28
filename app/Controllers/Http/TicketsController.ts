c922d0d7ec9e1058823e957498ef24ee92600caa
marc <rfxct@gmail.com>
2021-11-28 14:30:07 -0300

    logic to accept and finish tickets

--- app/Controllers/Http/TicketsController.ts
+++ app/Controllers/Http/TicketsController.ts
@@ -1,4 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CommonException from 'App/Exceptions/CommonException'
 import RankException from 'App/Exceptions/RankException'
 import Ticket from 'App/Models/Ticket'
 import CreateTicket from 'App/Validators/CreateTicketValidator'
@@ -30,12 +31,54 @@ export default class TicketsController {
     const ticket = await Ticket.findOrFail(id)
     await ticket.load('creator')
     await ticket.load('helper')
-    await ticket.load('messages', builder => builder.preload('author'))
 
     if (
       ticket.status === 'em curso' &&
       ![ticket.createdBy, ticket.assignedTo].includes(targetId)
     ) throw new RankException('Você não possui permissão para visualizar este ticket', 403, 'E_ACCES_DENIED')
+
+    return ticket
+  }
+
+  public async acceptTicket({ auth, params: { id: ticketId } }: HttpContextContract) {
+    const targetId = auth.use('api').user!.id
+
+    const alreadyAccepted = await Ticket.query()
+      .where('id', ticketId)
+      .whereRaw('assigned_to IS NOT NULL')
+      .first()
+    if (alreadyAccepted) throw new CommonException('Esse ticket já foi atribuido a outro ajudante', 403, 'E_ALREADY_ACCEPTED')
+
+    await Ticket.query()
+      .where('id', ticketId)
+      .whereRaw('assigned_to IS NULL')
+      .update({ assigned_to: targetId })
+
+    const ticket = await Ticket.query()
+      .where('id', ticketId)
+      .andWhere('assigned_to', targetId)
+      .preload('creator')
+      .preload('helper')
+      .first()
+    if (!ticket) throw new CommonException('Ocorreu um erro ao atribuir o ticket')
+
+    return ticket
+  }
+
+  public async finishTicket({ auth, params: { id: ticketId } }: HttpContextContract) {
+    const targetId = auth.use('api').user!.id
+
+    await Ticket.query()
+      .whereRaw('id = :ticketId AND (created_by = :targetId OR assigned_to = :targetId)', { targetId, ticketId })
+      .update({ finished: true })
+
+    const ticket = await Ticket.findOrFail(ticketId)
+    await ticket.load('creator')
+    await ticket.load('helper')
+
+    if (
+      ![ticket.createdBy, ticket.assignedTo].includes(targetId)
+    ) throw new RankException('Você não possui permissão para finalizar este ticket', 403, 'E_ACCES_DENIED')
 
     return ticket
   }
