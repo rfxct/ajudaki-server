5bf987ac1440059ab8e1aad40629dbddb456c7d2
marc <rfxct@gmail.com>
2021-11-28 14:30:32 -0300

    rewrite rank exception

--- app/Exceptions/RankException.ts
+++ app/Exceptions/RankException.ts
@@ -1,23 +1,11 @@
-import { Exception } from '@adonisjs/core/build/standalone'
+import CommonException from './CommonException'
 
-/*
-|--------------------------------------------------------------------------
-| Exception
-|--------------------------------------------------------------------------
-|
-| The Exception class imported from `@adonisjs/core` allows defining
-| a status code and error code for every exception.
-|
-| @example
-| new RankException('message', 500, 'E_RUNTIME_EXCEPTION')
-|
-*/
-export default class RankException extends Exception {
+export default class RankException extends CommonException {
   constructor(
     message: string = 'Você não possui permissão para executar esta ação',
     status: number = 403,
     code: string = 'E_INSUFICIENT_PERMISSION'
-    ) {
+  ) {
     super(message, status, code)
   }
 }
